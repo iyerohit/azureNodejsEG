2e7d3dc549faf8c44a53262f90dc85396b73d2e9
iyerohit <iyerohit@gmail.com>
2022-07-07 12:21:27 +0530

    hopefully success deploy by commit to git now at least

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home Automated Git Deployment' });
+});
+
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+  res.send('hellow world from committnig code to git and automated deployment to Azure cloud by app service');
+});
+
+
+/* GET error page. */
+router.get('/error', function(req, res, next) {
+  res.render('error',{message:'Error Message here',error:{stack:'error stack .',status:'4000'}});
+});
 module.exports = router;
